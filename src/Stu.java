34f7798b9d6b5ba009919aea2cec0d84edceb8d3
Jannu-T <jannuthiyagu@gmail.com>
2023-07-06 15:57:49 +0530

    first commit

---
+++ src/Stu.java
@@ -0,0 +1,16 @@
+public class Stu {
+    String name;
+    int rollno;
+    int age;
+
+    String grade;
+    String dept;
+    Stu(int rollno,String name,int age,String dept,String grade)
+    {
+        this.name=name;
+        this.rollno=rollno;
+        this.age=age;
+        this.dept=dept;
+        this.grade=grade;
+    }
+}
